c236eebdab3929365db0e37e763ea0d715462520
Mukund Lakshman <mukund@justin.tv>
2017-06-14 14:19:01 +0100

    Add image resizing

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'yaymukund-website',
+    artDir: 'art/',
     environment: environment
   };
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,32 @@
 'use strict';
 
+const path = require('path');
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
+const MergeTrees = require('broccoli-merge-trees');
+const BroccoliImageResize = require('broccoli-image-resize');
+const stew = require('broccoli-stew');
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
     // Add options here
   });
 
-  return app.toTree();
+  app = app.toTree();
+
+  // Generate thumbnails
+  let config = defaults.project.config(defaults.project.env);
+  let artSrcDir = path.join('public', config.artDir);
+
+  let thumbnails = new BroccoliImageResize([artSrcDir], {
+    withoutEnlargement: true,
+    sizes: {
+      default: [1080],
+      th: [100, 100]
+    },
+  });
+
+  thumbnails = stew.mv(thumbnails, config.artDir);
+  app = new MergeTrees([app, thumbnails], { overwrite: true });
+
+  return app;
 };
