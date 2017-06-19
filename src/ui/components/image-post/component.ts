1460af724d421a9576afc90bd239b461a7eed1d6
Mukund Lakshman <yaymukund@gmail.com>
2017-06-19 01:21:59 +0100

    Change the image to the thumbnail

--- src/ui/components/image-post/component.ts
+++ src/ui/components/image-post/component.ts
@@ -1,4 +1,11 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+
+const ART_DIR: string = '/art';
 
 export default class ImagePost extends Component {
+  @tracked('args')
+  get thumbnailUrl(): string {
+    let { name, ext } = this.args.image;
+    return `${ART_DIR}/${name}-th.${ext}`;
+  }
 };
